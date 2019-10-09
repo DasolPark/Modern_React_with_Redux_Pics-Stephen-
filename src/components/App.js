73cc7af19d15a03f6b08b212e721a2d6a0ab156c
Dasol Park <dasol.park615@gmail.com>
2019-10-09 18:05:06 +0900

    89. Axios vs Fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  onSearchSubmit(term) {}
 
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
+      <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
